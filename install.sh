0b8e254ed5a01f2e619b845755e389bb90574a49
Mika Andrianarijaona <mikaoelitiana@gmail.com>
2022-01-27 22:56:28 +0100

    feat: create install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+echo "Installing brew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "Installing fish"
+brew install fish
+echo $(which fish) >> /etc/shells
+
+echo "Iinstall asdf"
+brew install asdf
+brew install gpg gawk
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf install nodejs lts
+npm i -g yarn
+asdf global nodejs lts
+
