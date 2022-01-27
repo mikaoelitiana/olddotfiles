5e1dd5bbc9ed3f843b549ea4e39556bed29cbcfd
Mika Andrianarijaona <mandrianarijaona@alkemics.com>
2022-01-27 16:13:02 +0100

    feat: add updated configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# enable ASDF
 source /usr/local/opt/asdf/libexec/asdf.fish
+
+# config
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+
