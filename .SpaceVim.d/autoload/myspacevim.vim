b7576ce4fe809562ac3888a090431f1d8d9e5a54
Mika Andrianarijaona <mikaoelitiana@gmail.com>
2022-01-28 08:48:23 +0100

    rename install script

--- .SpaceVim.d/autoload/myspacevim.vim
+++ .SpaceVim.d/autoload/myspacevim.vim
@@ -3,8 +3,7 @@ function! myspacevim#before() abort
     let b:eslint_exe = substitute(system('npm bin') ,'\n', '', 'g') . '/eslint'
     let b:neomake_eslint_exe = b:eslint_exe
     let g:neomake_python_enabled_makers = ['flake8']
-
-
+    set shell=$HOMEBREW_REPOSITORY/bin/fish
 endfunction
 
 function! myspacevim#after() abort
--- install_maocs.sh
+++ install_maocs.sh
@@ -6,11 +6,14 @@ echo "Installing fish"
 brew install fish
 echo $(which fish) >> /etc/shells
 
-echo "Iinstall asdf"
+echo "Installing asdf"
 brew install asdf
 brew install gpg gawk
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf install nodejs lts
 npm i -g yarn
 asdf global nodejs lts
+
+echo "Installing neovim"
+brew install neovim
 
