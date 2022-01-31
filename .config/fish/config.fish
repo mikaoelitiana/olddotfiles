7d90095e912c0dbaa5a52cdea571d4e14d07395f
Mika Andrianarijaona <mandrianarijaona@alkemics.com>
2022-01-31 10:22:19 +0100

    fix: do not use asdf for nodejs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,9 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# enable ASDF
-source (brew --prefix asdf)/libexec/asdf.fish
-
 # config
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 set -gx EDITOR nvim
