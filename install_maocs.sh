fe3db9898b060ad9209568635da29e55fe09ac1d
Mika Andrianarijaona <mandrianarijaona@alkemics.com>
2022-04-13 16:19:56 +0200

    feat: add installations: - spacevim - prettier - eslint - gh - tmux - tmuxinator

--- install_maocs.sh
+++ install_maocs.sh
@@ -6,17 +6,25 @@ echo "Installing fish"
 brew install fish
 echo $(which fish) >> /etc/shells
 
-echo "Installung node via volta"
+echo "Installing node via volta"
 curl https://get.volta.sh | bash
 volta install node
-npm i -g yarn
-
-echo "Installing neovim"
-brew install neovim
-npm i -g neovim
-brew install ripgrep
+npm i -g yarn prettier eslint
 
 echo "Installing powerline"
 brew install python
 python -m pip install --user powerline-status
+
+echo "Installing neovim + spacevim"
+brew install neovim
+npm i -g neovim
+brew install ripgrep
+curl -sLf https://spacevim.org/install.sh | bash
+
+echo "Installing Git toolings"
+brew install gh
+
+echo "Installing tmux"
+brew install tmux
+brew install tmuxinator
 
