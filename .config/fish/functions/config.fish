302bd90363b321eaa4e3c35ae866e29fbf735f03
Mika Andrianarijaona <mikaoelitiana@gmail.com>
2022-06-29 18:37:50 +0200

    fix function

--- .config/fish/functions/config.fish
+++ .config/fish/functions/config.fish
@@ -1,4 +1,3 @@
 function config --wraps 'git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-    /* git --git-dir=$HOME/.cfg/ --work-tree=$HOME $argv */
     lazygit --git-dir=$HOME/.cfg/ --work-tree=$HOME $argv
 end
