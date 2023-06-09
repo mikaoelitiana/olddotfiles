4b0abd9879b22773e345ffd3190653d05ad0761a
Mika Andrianarijaona <mika.andrianarijaona@busbud.com>
2023-06-09 15:52:11 +0200

    feat: add context plugin

---
+++ .config/nvim/lua/plugins/editor.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    event = "VeryLazy",
+    opts = {
+      enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+      max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+      min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+      line_numbers = true,
+      multiline_threshold = 20, -- Maximum number of lines to collapse for a single context line
+      trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+      mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+      -- Separator between context and content. Should be a single character string, like '-'.
+      -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+      separator = nil,
+      zindex = 20, -- The Z-index of the context window
+    },
+    keys = {
+      {
+        "<leader>cc",
+        "<cmd>TSContextToggle<cr>",
+        desc = "Toggle context",
+      },
+      {
+        "<leader>cg",
+        function()
+          require("treesitter-context").go_to_context()
+        end,
+        desc = "Go to context",
+      },
+      {
+        "gC",
+        function()
+          require("treesitter-context").go_to_context()
+        end,
+        desc = "Go to context",
+      },
+    },
+  },
+}
