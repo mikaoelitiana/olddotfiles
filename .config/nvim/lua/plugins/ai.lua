65479251df7b72606d3812f1976ffebeb7b4019f
Mika Andrianarijaona <mika.andrianarijaona@busbud.com>
2023-07-05 15:01:07 +0200

    fix: codeium autocomplete

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,3 +1,13 @@
 return {
-  { "Exafunction/codeium.vim" },
+  {
+    "jcdickinson/codeium.nvim",
+    commit = "b1ff0d6c993e3d87a4362d2ccd6c660f7444599f",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+    },
+    config = function()
+      require("codeium").setup({})
+    end,
+  },
 }
