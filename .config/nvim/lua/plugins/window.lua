d5b314539236b81a649ac555e2656dddfe801f55
Mika Andrianarijaona <mika.andrianarijaona@busbud.com>
2023-09-05 11:29:17 +0200

    Update buffer config to display numbers

--- .config/nvim/lua/plugins/window.lua
+++ .config/nvim/lua/plugins/window.lua
@@ -27,4 +27,98 @@ return {
       },
     },
   },
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
+      { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
+      {
+        "<leader>1",
+        function()
+          require("bufferline").go_to(1, true)
+        end,
+        desc = "Jump to buffer 1",
+      },
+      {
+        "<leader>2",
+        function()
+          require("bufferline").go_to(2, true)
+        end,
+        desc = "Jump to buffer 2",
+      },
+      {
+        "<leader>3",
+        function()
+          require("bufferline").go_to(3, true)
+        end,
+        desc = "Jump to buffer 3",
+      },
+      {
+        "<leader>4",
+        function()
+          require("bufferline").go_to(4, true)
+        end,
+        desc = "Jump to buffer 4",
+      },
+      {
+        "<leader>5",
+        function()
+          require("bufferline").go_to(5, true)
+        end,
+        desc = "Jump to buffer 5",
+      },
+      {
+        "<leader>6",
+        function()
+          require("bufferline").go_to(6, true)
+        end,
+        desc = "Jump to buffer 6",
+      },
+      {
+        "<leader>7",
+        function()
+          require("bufferline").go_to(7, true)
+        end,
+        desc = "Jump to buffer 7",
+      },
+      {
+        "<leader>8",
+        function()
+          require("bufferline").go_to(8, true)
+        end,
+        desc = "Jump to buffer 8",
+      },
+      {
+        "<leader>9",
+        function()
+          require("bufferline").go_to(9, true)
+        end,
+        desc = "Jump to buffer 9",
+      },
+    },
+    opts = {
+      options = {
+        diagnostics = "nvim_lsp",
+        always_show_bufferline = false,
+        show_buffer_close_icons = false,
+        numbers = "ordinal",
+        mappings = true,
+        diagnostics_indicator = function(_, _, diag)
+          local icons = require("lazyvim.config").icons.diagnostics
+          local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+            .. (diag.warning and icons.Warn .. diag.warning or "")
+          return vim.trim(ret)
+        end,
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "Neo-tree",
+            highlight = "Directory",
+            text_align = "left",
+          },
+        },
+      },
+    },
+  },
 }
