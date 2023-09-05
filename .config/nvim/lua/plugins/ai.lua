4aace513ac2175ad9e50e5eee64e63931f976c72
Mika Andrianarijaona <mika.andrianarijaona@busbud.com>
2023-09-05 11:29:00 +0200

    Remove codeium version pin

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,7 +1,6 @@
 return {
   {
     "jcdickinson/codeium.nvim",
-    commit = "b1ff0d6c993e3d87a4362d2ccd6c660f7444599f",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "hrsh7th/nvim-cmp",
