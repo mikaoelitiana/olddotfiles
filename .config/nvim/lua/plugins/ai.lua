cb5683064c784e3a97f4f721c24861e585e8690e
Mika Andrianarijaona <mika.andrianarijaona@busbud.com>
2024-03-18 16:31:09 +0100

    feat: add CopilotChat.nvim

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -9,4 +9,15 @@ return {
       require("codeium").setup({})
     end,
   },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" },
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    opts = {
+      debug = false,
+    },
+  },
 }
