c98773b7bc30528c7450fcc1a36f7ffa0b7de314
Mika Andrianarijaona <mika.andrianarijaona@busbud.com>
2024-03-18 15:15:51 +0100

    fix: re-add codeium

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "plugins" },
   },
   defaults = {
